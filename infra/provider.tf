6cad748914d4fc8beb1b837499286e44b6a267f6
Cameron Guilbeau <cameroncarlg@gmail.com>
2023-06-18 12:46:21 -0500

    added IaC

---
+++ infra/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+}
